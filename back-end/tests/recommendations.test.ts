f70bc29468589824543a0b706795ff4d84004b1a
barbararech <rech.brb@gmail.com>
2022-09-21 09:50:23 -0300

    test: implements post duplicated recommendation

--- back-end/tests/recommendations.test.ts
+++ back-end/tests/recommendations.test.ts
@@ -19,6 +19,15 @@ describe("Test POST /recommendations", () => {
     expect(result.status).toBe(201);
     expect(createdMusic).toBeInstanceOf(Object);
   });
+
+  it("Should return 409 if registered a recommendation that already exists", async () => {
+    const music = await recommendationFactory();
+
+    await supertest(app).post(`/recommendations`).send(music);
+    const result = await supertest(app).post(`/recommendations`).send(music);
+
+    expect(result.status).toBe(409);
+  });
 });
 
 afterAll(async () => {
