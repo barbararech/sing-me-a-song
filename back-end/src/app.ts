1b0178dee2d7199733076dabf0aa52494296b52b
barbararech <rech.brb@gmail.com>
2022-09-25 08:55:04 -0300

    test: fix e2e route to e2e test

--- back-end/src/app.ts
+++ back-end/src/app.ts
@@ -4,6 +4,9 @@ import "express-async-errors";
 import { errorHandlerMiddleware } from "./middlewares/errorHandlerMiddleware.js";
 import recommendationRouter from "./routers/recommendationRouter.js";
 import e2eRouter from "./routers/e2eRouter.js";
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const app = express();
 app.use(cors());
--- back-end/src/repositories/e2eRepository.ts
+++ back-end/src/repositories/e2eRepository.ts
@@ -1,5 +1,5 @@
 import { prisma } from "../database.js";
 
 export async function truncate() {
-  await prisma.$executeRaw`TRUNCATE TABLE users`;
+  await prisma.$executeRaw`TRUNCATE TABLE recommendations`;
 }
